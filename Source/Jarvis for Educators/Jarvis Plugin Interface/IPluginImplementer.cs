8c25f0c3c6e6c271d0c975bc72cfc0f6955f2fa8
Janynne Gomes <janynnegomes@gmail.com>
2016-07-10 14:27:14 -0300

    Created a plugin interface to allow external DLL be incorporated to the software
    Using MEF framework

---
+++ Source/Jarvis for Educators/Jarvis Plugin Interface/IPluginImplementer.cs
@@ -0,0 +1,23 @@
+﻿using Jarvis_Library;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace JarvisPluginInterface
+{
+   public interface IPluginImplementer
+    {
+
+        string PluginName();
+        Icon PluginIcon();
+        void MenuAdder(SideMenuItem menuItem);
+
+        Form LoadContent();
+
+    }
+}
